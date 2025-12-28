c5a528e4bb1fb6dde0d79b37b15b0710f5ff429e
Айжигит Дюшенгазиев <dusengazievajzigit@gmail.com>
2025-12-28 23:05:07 +0600

    Refactor main method in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        Krug k = new Krug(5); // радиус = 5
+        Krug k = new Krug(5); 
         System.out.println("Площадь: " + k.ploshchad());
         System.out.println("Окружность: " + k.okruzhnost());
-
-        // Вызов статических методов
+        
         System.out.println("Площадь (статический метод): " + Krug.ploshchadStatic(5));
         System.out.println("Окружность (статический метод): " + Krug.okruzhnostStatic(5));
     }
