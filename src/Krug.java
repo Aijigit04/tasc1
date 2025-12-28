0c0379a50a60000bf5b5dbcae4ee5610706f90eb
Айжигит Дюшенгазиев <dusengazievajzigit@gmail.com>
2025-12-28 23:06:15 +0600

    Clean up comments in Krug.java
    Removed comments for properties and methods.

--- src/Krug.java
+++ src/Krug.java
@@ -1,24 +1,19 @@
 public class Krug {
-    // Поля (свойства)
     double radius;
     static final double PI = 3.14159265359;
 
-    // Конструктор
     public Krug(double radius) {
         this.radius = radius;
     }
-
-    // Метод для вычисления площади круга
+    
     public double ploshchad() {
         return PI * radius * radius;
     }
 
-    // Метод для вычисления длины окружности
     public double okruzhnost() {
         return 2 * PI * radius;
     }
 
-    // Статический метод (можно вызвать без объекта)
     public static double ploshchadStatic(double r) {
         return PI * r * r;
     }
